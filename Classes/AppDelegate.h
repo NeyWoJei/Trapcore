39e48a1f0fc2d5e24eff22320dc04363edc94113
JekaJeka0 <isakoveugene0@gmail.com>
2024-11-28 21:03:17 +0400

    0.0.1 - Исправлена структура проекта

---
+++ Classes/AppDelegate.h
@@ -0,0 +1,21 @@
+﻿#ifndef  _APP_DELEGATE_H_
+#define  _APP_DELEGATE_H_
+
+#include "cocos2d.h"
+
+class AppDelegate : private cocos2d::Application
+{
+public:
+    AppDelegate();  // Конструктор
+    virtual ~AppDelegate();  // Деструктор
+
+    virtual bool applicationDidFinishLaunching();  // Инициализация приложения
+    virtual void applicationDidEnterBackground();  // Приложение ушло в фон
+    virtual void applicationWillEnterForeground();  // Приложение вернулось в активное состояние
+
+    void initGLContextAttrs();  // Настройка атрибутов OpenGL
+};
+
+
+#endif // _APP_DELEGATE_H_
+
